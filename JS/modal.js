8c2e7268342e1a25527ed26563e0467bc59601ca
Valeriia-Kozlova <jokkerav7o7@gmail.com>
2020-07-02 00:19:23 +0300

    02-07-2020

--- JS/modal.js
+++ JS/modal.js
@@ -1,11 +1,13 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-open-modal]"),
-    closeModalBtn: document.querySelector("[data-close-modal]"),
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
   };
+
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
+
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
   }
